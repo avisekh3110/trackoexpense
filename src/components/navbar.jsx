0981ca0290d1e6c4a9aa0e17e83b6f7266319dd3
avisekh3110 <avisekh.sabi31102003@gmail.com>
2025-01-13 20:47:40 +0530

    {"C":["home.jsx","navbar.jsx"]}

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import burger from "/burger.svg";
 
-export default function Navbar({ checkActive, setCheckActive }) {
+export default function Navbar() {
+  const [checkActive, setCheckActive] = useState(true);
   return (
-    <div className="bg-primary-b w-full flex justify-between items-center lg:px-20 p-6 z-10">
-      <div className="text-tertiary-a text-xl tracking-wide font-bold">
-        TRACKOEXPENSE
-      </div>
-      <div className="bg-primary-a lg:w-1/6 sm:w-1/3 w-14  rounded-full sm:rounded-lg  flex gap-10 justify-around sm:px-8 items-center text-tertiary-a font-semibold h-10">
-        <Link className="hidden sm:flex" to={"/"}>
-          HOME
-        </Link>
-        <Link className="hidden sm:flex" to={"/support"}>
-          SUPPORT
-        </Link>
-        <div
-          className="sm:hidden flex justify-center"
-          onClick={() => {
-            setCheckActive(!checkActive);
-          }}
-        >
-          <img height={20} width={20} src={burger} alt="Ham" />
+    <>
+      <div className="bg-primary-b w-full flex justify-between items-center lg:px-20 p-6">
+        <div className="text-tertiary-a text-xl tracking-wide font-bold">
+          TRACKOEXPENSE
+        </div>
+        <div className="bg-primary-a lg:w-1/6 sm:w-1/3 w-14  rounded-full sm:rounded-lg  flex gap-10 justify-around sm:px-8 items-center text-tertiary-a font-semibold h-10">
+          <Link className="hidden sm:flex" to={"/"}>
+            HOME
+          </Link>
+          <Link className="hidden sm:flex" to={"/support"}>
+            SUPPORT
+          </Link>
+          <div
+            className="sm:hidden flex justify-center"
+            onClick={() => {
+              setCheckActive(!checkActive);
+            }}
+          >
+            <img height={20} width={20} src={burger} alt="Ham" />
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        className={`absolute bg-tertiary-b ${
+          checkActive ? "-translate-y-[15rem]" : ""
+        } transform p-3 w-full duration-300 ease-out shadow-slate-900 shadow-lg`}
+      >
+        <div className="h-full w-full flex flex-col justify-center items-center gap-4 font-bold tracking-widest text-primary-b">
+          <Link to={"/"}>HOME</Link>
+          <hr className="w-full bg-black" />
+          <Link to={"/support"}>SUPPORT</Link>
+        </div>
+      </div>
+    </>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import Navbar from "../components/navbar";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const [checkActive, setCheckActive] = useState(true);
   return (
     <div className="w-full bg-slate-300 h-screen">
-      <Navbar checkActive={checkActive} setCheckActive={setCheckActive} />
-      <div
-        className={`absolute bg-white ${
-          checkActive ? "-translate-x-[30rem]" : "-translate-x-[0rem]"
-        } transform  h-1/3 w-full duration-300 ease-out opacity-30`}
-      >
-        <div className="h-full w-full blur-lg"></div>
+      <Navbar />
+      <div className="bg-gradient-to-b from-primary-b via-primary-b to-primary-a h-1/3 w-full flex">
+        <div className="bg-white h-full w-1/2">
+          <div className="bg-blue-200 w-full h-1/2 flex justify-center items-center">
+            <div className="font-black text-2xl">TRACK YOUR EXPENSES</div>
+            <div></div>
+          </div>
+          <div className="bg-yellow-200 w-full h-1/2"></div>
+        </div>
+        <div className="bg-slate-200 h-full w-1/2"></div>
       </div>
-      <div className="bg-gradient-to-b from-primary-b via-primary-b to-primary-a h-1/3 w-full"></div>
     </div>
   );
 }
